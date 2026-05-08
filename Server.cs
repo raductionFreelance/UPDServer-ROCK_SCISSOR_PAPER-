4bcce84d35bd3a98d002e8314d11ca5a7107f1a2
VITALII HURIN <raductionshop@gmail.com>
2026-05-08 20:21:33 +0300

    Refactor UpdClientServer to UpdServer with improvements

--- Server.cs
+++ Server.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-using System.Diagnostics; 
+using System.Diagnostics;
 using System.Collections.Generic;
-using System.Linq; 
+using System.Linq;
 
-class UpdClientServer
+class UpdServer
 {
     static Random random = new Random();
 
@@ -16,91 +16,75 @@ class UpdClientServer
         int port = 1200;
         using (UdpClient udpServer = new UdpClient(port))
         {
-            Console.WriteLine($"[UDP] Сервер запущено. Очікування вибору режиму...");
+            Console.WriteLine("--- НАЛАШТУВАННЯ СЕРВЕРА ---");
+            Console.WriteLine("Виберіть режим для СЕРВЕРА: 1 - Людина (ви вводите хід), 2 - Бот");
+            int serverMode = int.Parse(Console.ReadLine() ?? "2");
 
-            UdpReceiveResult initResult = await udpServer.ReceiveAsync();
-            int gameMode = int.Parse(Encoding.UTF8.GetString(initResult.Buffer));
-            IPEndPoint clientEndPoint = initResult.RemoteEndPoint;
+            Console.WriteLine($"\n[UDP] Сервер слухає на порту {port}. Очікування першого ходу від клієнта...");
 
             int[] matchStats = new int[4]; 
             List<string> gameSummaries = new List<string>();
+            IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
             for (int g = 1; g <= 3; g++)
             {
-                Console.WriteLine($"\n--- ПОЧАТОК ГРИ №{g} ---");
                 Stopwatch gameTimer = Stopwatch.StartNew();
-                int playerWins = 0;
-                int computerWins = 0;
+                int playerWins = 0, computerWins = 0;
 
                 for (int r = 1; r <= 5; r++)
                 {
-                    UdpReceiveResult roundResult = await udpServer.ReceiveAsync();
-                    int playerMove = int.Parse(Encoding.UTF8.GetString(roundResult.Buffer));
-                    clientEndPoint = roundResult.RemoteEndPoint;
+                    UdpReceiveResult result = await udpServer.ReceiveAsync();
+                    clientEndPoint = result.RemoteEndPoint;
+                    int playerMove = int.Parse(Encoding.UTF8.GetString(result.Buffer));
 
-                    int serverMove = GenerataChoice(gameMode);
+                    int serverMove = GenerateServerChoice(serverMode);
 
                     if (playerMove >= 1 && playerMove <= 3) matchStats[playerMove]++;
                     if (serverMove >= 1 && serverMove <= 3) matchStats[serverMove]++;
 
-                    string resultText = "";
-                    if (playerMove == 4) { resultText = "Гравець здався!"; computerWins = 99; break; }
+                    string roundResult = "";
+                    if (playerMove == 4) { roundResult = "Гравець здався!"; computerWins = 99; break; }
 
-                    if (playerMove == serverMove) resultText = "Нічия!";
-                    else if ((playerMove == 1 && serverMove == 2) ||
-                             (playerMove == 2 && serverMove == 3) ||
-                             (playerMove == 3 && serverMove == 1))
+                    if (playerMove == serverMove) roundResult = "Нічия!";
+                    else if ((playerMove == 1 && serverMove == 2) || (playerMove == 2 && serverMove == 3) || (playerMove == 3 && serverMove == 1))
                     {
-                        resultText = "Ви виграли раунд!";
+                        roundResult = "Клієнт виграв раунд!";
                         playerWins++;
                     }
                     else
                     {
-                        resultText = "Сервер виграв раунд!";
+                        roundResult = "Сервер виграв раунд!";
                         computerWins++;
                     }
 
-                    string roundStats = $"[Раунд {r}] Ви: {MoveName(playerMove)}, Сервер: {MoveName(serverMove)}. {resultText}";
-                    Console.WriteLine(roundStats);
-                    await Send(udpServer, roundStats, clientEndPoint);
+                    string roundMessage = $"[Р{r}] Клієнт: {MoveName(playerMove)}, Сервер: {MoveName(serverMove)}. {roundResult}";
+                    Console.WriteLine(roundMessage);
+                    await Send(udpServer, roundMessage, clientEndPoint);
                 }
 
                 gameTimer.Stop();
-                string gameResult = playerWins > computerWins ? "ПЕРЕМОГА ГРАВЦЯ" : "ПЕРЕМОГА СЕРВЕРА";
-                string gameStat = $"Результат гри №{g}: {gameResult} ({playerWins}:{computerWins}). Час: {gameTimer.Elapsed.Seconds} сек.";
-
-                gameSummaries.Add(gameStat);
-                Console.WriteLine(gameStat);
-                await Send(udpServer, gameStat, clientEndPoint);
+                string summary = $"Гра №{g}: {(playerWins > computerWins ? "Клієнт" : "Сервер")} виграв ({playerWins}:{computerWins}). Час: {gameTimer.Elapsed.Seconds}с";
+                gameSummaries.Add(summary);
+                await Send(udpServer, summary, clientEndPoint);
             }
 
-            string finalMatchStats = "\n=== СТАТИСТИКА МАТЧУ ===\n" + string.Join("\n", gameSummaries);
-
+            string finalStats = "\n=== ПІДСУМОК МАТЧУ ===\n" + string.Join("\n", gameSummaries);
             int maxUse = matchStats.Skip(1).Max();
-            int minUse = matchStats.Skip(1).Min();
-            string popular = MoveName(Array.IndexOf(matchStats, maxUse));
-            string unpopular = MoveName(Array.LastIndexOf(matchStats, minUse));
-
-            finalMatchStats += $"\nНайпопулярніша фігура: {popular}\nНайнепопулярніша: {unpopular}";
-
-            Console.WriteLine(finalMatchStats);
-            await Send(udpServer, finalMatchStats, clientEndPoint);
+            finalStats += $"\nНайпопулярніша фігура: {MoveName(Array.IndexOf(matchStats, maxUse))}";
+            
+            await Send(udpServer, finalStats, clientEndPoint);
+            Console.WriteLine("Матч завершено. Статистику відправлено.");
         }
     }
 
-    static async Task Send(UdpClient s, string msg, IPEndPoint ep)
-    {
-        byte[] data = Encoding.UTF8.GetBytes(msg);
-        await s.SendAsync(data, data.Length, ep);
-    }
+    static async Task Send(UdpClient s, string msg, IPEndPoint ep) =>
+        await s.SendAsync(Encoding.UTF8.GetBytes(msg), Encoding.UTF8.GetByteCount(msg), ep);
 
-    static string MoveName(int m) => m switch { 1 => "Камінь", 2 => "Ножиці", 3 => "Папір", _ => "Невідомо" };
+    static string MoveName(int m) => m switch { 1 => "Камінь", 2 => "Ножиці", 3 => "Папір", _ => "Здача" };
 
-    public static int GenerataChoice(int i)
-    {
-        if (i == 1)
-        {
-            Console.WriteLine("Ваш хід (1-К, 2-Н, 3-П): ");
+    static int GenerateServerChoice(int mode) {
+        if (mode == 1) {
+            Console.Write("Ваш хід (1-К, 2-Н, 3-П): ");
             return int.Parse(Console.ReadLine() ?? "1");
         }
         return random.Next(1, 4);
